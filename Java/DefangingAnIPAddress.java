3cee99f2dbffc784dcde450c4b24576c00760170
Hazlenut <38819915+Hazlenut@users.noreply.github.com>
2020-02-04 22:38:23 -0500

    Create DefangingAnIPAddress.java

---
+++ Java/DefangingAnIPAddress.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/defanging-an-ip-address/
+class Solution {
+    public String defangIPaddr(String address) {
+        String result = "";
+        for(int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                result += "[.]";
+            }else{
+                result += address.charAt(i);
+            }
+        }   
+        return result;
+    }
+}
